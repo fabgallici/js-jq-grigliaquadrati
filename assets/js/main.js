6961b784e2134a0bcf1b1e07421cc386e30456c4
cabal82 <fabriziogallici@gmail.com>
2019-11-06 15:40:22 +0100

    es. base

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-
 $(document).ready(function () {
-  
+
+  var redScore = 0;
+  var greenScore = 0;
+
   $(".square").click(function() {
     if($(this).hasClass("red")) {
       $(this).css("background", "red");
+      redScore++;
+      $(".red-counter > span").text(redScore);
+      $(this).off();
     } else {
       $(this).css("background", "green");
+      greenScore++;
+      $(".green-counter > span").text(greenScore);
+      $(this).off();
     }
   });
 
