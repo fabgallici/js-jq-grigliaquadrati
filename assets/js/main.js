ebda457dcdb08fe32574a902ee633ecb1c0d2517
cabal82 <fabriziogallici@gmail.com>
2019-11-06 15:12:48 +0100

    green square

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+
 $(document).ready(function () {
   
   $(".square").click(function() {
     if($(this).hasClass("red")) {
       $(this).css("background", "red");
+    } else {
+      $(this).css("background", "green");
     }
   });
 
